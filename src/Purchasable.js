8ab2ed0d110ac37162f1bbb0ac82e559d75cd5a5
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2021-04-18 00:49:26 -0500

    Change "Purchasable" container <div> to <button>

--- src/Purchasable.js
+++ src/Purchasable.js
@@ -4,13 +4,13 @@ import logo from './logo.svg';
 function Purchasable({label, item, cost, keyBinding, onClickUnowned, onClickOwned}) {
   const owned = cost === "OWNED";
   return (
-    <div className="Purchasable" onClick={owned ? onClickOwned : () => onClickUnowned(item, cost)}>
+    <button className="Purchasable" onClick={owned ? onClickOwned : () => onClickUnowned(item, cost)}>
       <div className="Purchasable-icon-container">
         <img src={logo} className="Purchasable-icon" alt={label} />
         {owned && <span className="fas fa-check-square Purchasable-checkmark"></span>}
       </div>
       <p className="Purchasable-label">{label}: {owned ? keyBinding : cost}</p>
-    </div>
+    </button>
   );
 }
 
