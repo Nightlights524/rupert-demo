5816fd9e48c206f03c61b30f0bd3daab81ec9c9b
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2021-07-23 22:24:00 -0500

    Delete commented-out code

--- client/src/RupertApp/Sidebar/Sidebar.js
+++ client/src/RupertApp/Sidebar/Sidebar.js
@@ -2,37 +2,10 @@ import React from 'react';
 import PropTypes from "prop-types"
 import * as styles from "./Sidebar.module.css"
 
-// function Sidebar({children}) {
-//   return (
-//     <div className={styles.sidebar}>
-//       {children}
-//     </div>
-//   );
-// }
-
 const Sidebar = ({sidebarOpen, setSidebarOpen = null, children}) => {
-  // const node = useRef();
-  // useOnClickOutside(node, () => setSidebarOpen(false));
-
-  // useEffect(() => {
-  //   document.body.style.overflow = 'hidden';
-  //   return () => document.body.style.overflow = 'unset';
-  // }, []);
-
-  // const isHidden = sidebarOpen ? true : false;
-  // const tabIndex = sidebarOpen ? 0 : -1;
-
   const animationStyle = sidebarOpen ? {transform: 'translateX(0)'} : {transform: 'translateX(-100%)'};
 
   return (
-    // <nav aria-hidden={!sidebarOpen} className={styles.sidebar} style={animationStyle}>
-    //   <NavLinks
-    //     vertical
-    //     tabIndex={sidebarOpen ? 0 : -1}
-    //     linkStyle={styles.link}
-    //     linkSideEffect={() => setSidebarOpen(false)}
-    //   />
-    // </nav>
     <div aria-hidden={!sidebarOpen} className={styles.sidebar} style={animationStyle}>
       {children}
     </div>
